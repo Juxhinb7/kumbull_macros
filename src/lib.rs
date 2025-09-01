58c82f1c369d78bec3856f29421c1b58b34c5dcd
juxhinb7 <juxhinbazelli@gmail.com>
2025-09-01 22:17:32 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemStruct};
+
+#[proc_macro_attribute]
+pub fn component(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input as a struct
+    let input = parse_macro_input!(item as syn::ItemStruct);
+
+    let struct_name = &input.ident;
+
+    let expanded = quote! {
+        #input
+
+        impl kumbull::Component #struct_name {
+            pub fn type_name() -> &'static str {
+                stringify!(#ident)
+            }   
+        }
+    };
+}
