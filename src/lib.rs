4c93cbb4faeaaf9d9aa306ac9865795ddb88ade3
juxhinb7 <juxhinbazelli@gmail.com>
2025-09-01 22:27:05 +0200

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,16 @@ use syn::{parse_macro_input, ItemStruct};
 #[proc_macro_attribute]
 pub fn component(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the input as a struct
-    let input = parse_macro_input!(item as syn::ItemStruct);
+    let input = parse_macro_input!(item as ItemStruct);
 
     let struct_name = &input.ident;
 
     let expanded = quote! {
         #input
 
-        impl kumbull::Component #struct_name {
+        impl kumbull::Component for #struct_name {
             pub fn type_name() -> &'static str {
-                stringify!(#ident)
+                stringify!(#struct_name)
             }   
         }
     };
